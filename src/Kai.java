cde2dfec24872e50fcecd2cd258733bb6f1b1a20
kaijjc <kai080729@gmail.com>
2024-11-21 15:34:47 +0900

    ファイル作成

---
+++ src/Kai.java
@@ -0,0 +1,7 @@
+class Kai {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello,evening");
+        System.out.println("アあa");
+    }
+}
